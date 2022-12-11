4698f2ff2e6c985dd412698adf63314f595cd926
Andrew Miller <andrew.r.miller45@gmail.com>
2022-12-11 09:58:48 -0800

    easy lvl 8

--- CodeWars/odd-or-even.js
+++ CodeWars/odd-or-even.js
@@ -3,3 +3,18 @@
 //Given a list of integers, determine whether the sum of its elements is odd or even.
 //Give your answer as a string matching "odd" or "even".
 //If the input array is empty consider it as: [0] (array with a zero).
+
+const oddOrEven = array => {
+  
+    let sum
+    
+    if(array.length){
+        sum = array.reduce((acc, cur) => acc + cur)
+    } else 
+        sum = 0
+  
+     if(sum % 2 === 0){
+       return 'even'
+     } else 
+       return 'odd'
+  }
